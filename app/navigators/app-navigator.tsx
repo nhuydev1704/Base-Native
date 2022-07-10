c4d0e80d56a9921c6a1f2f8fc9feafce10afbaf3
nhuydev1704 <khanhandli@gmail.com>
2022-07-10 18:09:23 +0700

    animated tab 2

--- app/navigators/app-navigator.tsx
+++ app/navigators/app-navigator.tsx
@@ -1,6 +1,6 @@
 import { DarkTheme, DefaultTheme, NavigationContainer } from "@react-navigation/native"
 import React from "react"
-import { StyleSheet, TouchableOpacity, useColorScheme } from "react-native"
+import { StyleSheet, TouchableOpacity, useColorScheme, View } from "react-native"
 import { navigationRef, useBackButtonHandler } from "./navigation-utilities"
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
@@ -47,34 +47,55 @@ const BottomTabArr = [
 // bottom tab
 
 const Tab = createBottomTabNavigator<NavigatorBottomParamList>()
+const animate1 = {
+  0: { scale: 0.5, translateY: 7 },
+  0.92: { translateY: -34 },
+  1: { scale: 1.2, translateY: -24 },
+}
+const animate2 = { 0: { scale: 1.2, translateY: -24 }, 1: { scale: 1, translateY: 7 } }
+
+const circle1 = {
+  0: { scale: 0 },
+  0.3: { scale: 0.3 },
+  0.5: { scale: 0.5 },
+  0.8: { scale: 0.7 },
+  1: { scale: 1 },
+}
+const circle2 = { 0: { scale: 1 }, 1: { scale: 0 } }
 
 const TabButton = (props: any) => {
   const { item, onPress, accessibilityState } = props
   const focused = accessibilityState.selected
   const viewRef = React.useRef(null)
+  const circleRef = React.useRef(null)
+  const textRef = React.useRef(null)
 
   React.useEffect(() => {
     if (focused) {
-      viewRef.current.animate({
-        0: { scale: 0.5, rotate: "0deg" },
-        1: { scale: 1.5, rotate: "360deg" },
-      })
+      viewRef.current.animate(animate1)
+      circleRef.current.animate(circle1)
+      textRef.current.transitionTo({ scale: 1 })
     } else {
-      viewRef.current.animate({
-        0: { scale: 1.5, rotate: "360deg" },
-        1: { scale: 1, rotate: "0deg" },
-      })
+      viewRef.current.animate(animate2)
+      circleRef.current.animate(circle2)
+      textRef.current.transitionTo({ scale: 0 })
     }
   }, [focused])
 
   return (
     <TouchableOpacity onPress={onPress} activeOpacity={1} style={styles.container}>
-      <Animatable.View ref={viewRef} duration={1000} style={styles.container}>
-        <IconSvg
-          type={item.type}
-          name={focused ? item.activeIcon : item.inActiveIcon}
-          color={focused ? Colors.primary : Colors.primaryLite}
-        />
+      <Animatable.View ref={viewRef} duration={500} style={styles.container}>
+        <View style={styles.btn}>
+          <Animatable.View ref={circleRef} style={styles.circle} />
+          <IconSvg
+            type={item.type}
+            name={item.inActiveIcon}
+            color={focused ? Colors.white : Colors.primary}
+          />
+        </View>
+        <Animatable.Text ref={textRef} style={styles.text}>
+          {item.label}
+        </Animatable.Text>
       </Animatable.View>
     </TouchableOpacity>
   )
@@ -85,14 +106,7 @@ const BottomStack = () => {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
-        tabBarStyle: {
-          height: 60,
-          position: "absolute",
-          bottom: 16,
-          right: 16,
-          left: 16,
-          borderRadius: 16,
-        },
+        tabBarStyle: styles.tabBar,
       }}
     >
       {BottomTabArr.map((item, index) => {
@@ -154,5 +168,35 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  tabBar: {
+    height: 70,
+    position: "absolute",
+    bottom: 16,
+    right: 16,
+    left: 16,
+    borderRadius: 16,
+  },
+  btn: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 4,
+    borderColor: Colors.white,
+    backgroundColor: Colors.white,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  circle: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: Colors.primary,
+    borderRadius: 25,
+  },
+  text: {
+    fontSize: 10,
+    textAlign: "center",
+    color: Colors.primary,
   },
 })
